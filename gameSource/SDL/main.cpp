a129a2b185933c89b03fdac95164fb7985d3422b
Jason Rohrer <jasonrohrer@fastmail.fm>
2009-09-09 20:27:48 +0000

    Got sprite transparency working.

--- gameSource/SDL/main.cpp
+++ gameSource/SDL/main.cpp
@@ -353,7 +353,8 @@ void callbackDisplay() {
     glDisable( GL_TEXTURE_2D );
 	glDisable( GL_CULL_FACE );
     glDisable( GL_DEPTH_TEST );
-
+    glEnable( GL_BLEND );
+    glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
