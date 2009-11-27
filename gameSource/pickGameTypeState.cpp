c1ca413077e0e0747490c64a9f3e0cf311369746
Jason Rohrer <jasonrohrer@fastmail.fm>
2009-11-27 20:09:12 +0000

    Added missing state.

---
+++ gameSource/pickGameTypeState.cpp
@@ -0,0 +1,173 @@
+#include "GameState.h"
+#include "Button.h"
+#include "common.h"
+#include "gameStats.h"
+#include "units.h"
+#include "colors.h"
+
+#include "minorGems/util/stringUtils.h"
+
+
+static char stateDone = false;
+
+extern int satelliteTopSpriteID;
+extern int satelliteBottomSpriteID;
+extern int satelliteBottomHalfOffset;
+
+extern int titleSpriteID[3];
+extern unsigned char titleFade;
+
+
+extern Button *aiButton;
+extern Button *wifiButton;
+extern char *statusMessage;
+extern char *statusSubMessage;
+
+extern char networkOpponent;
+
+
+
+class PickGameTypeState : public GameState {
+    public:
+        PickGameTypeState();
+        
+        virtual void clickState( int inX, int inY );
+        
+
+        virtual void stepState();
+        
+        
+
+        // draws into bottom screen
+        virtual void drawState();
+
+
+        virtual void enterState();        
+        virtual char isStateDone() {
+            return stateDone;
+            }
+        
+        
+
+        
+        virtual ~PickGameTypeState();
+        
+    protected:
+        char *mMessage;
+    };
+
+
+PickGameTypeState::PickGameTypeState() 
+        : mMessage( NULL ) {
+    }
+
+PickGameTypeState::~PickGameTypeState() {
+    
+    if( mMessage != NULL ) {
+        delete [] mMessage;
+        }
+    }
+
+static char selectionMade = false;
+
+
+
+void PickGameTypeState::clickState( int inX, int inY ) {
+
+ 
+    if( ! selectionMade ) {
+        if( aiButton->getPressed( inX, inY ) ) {
+            selectionMade = true;
+            
+            networkOpponent = false;
+            initOpponent( true );
+            }
+        else if( wifiButton->getPressed( inX, inY ) ) {
+            selectionMade = true;
+            
+            networkOpponent = true;
+            initOpponent( false );
+            }
+        }
+    }
+
+
+
+void PickGameTypeState::stepState() {
+
+    if( selectionMade ) {
+        if( titleFade > 0 ) {
+        
+            if( titleFade >= 8 ) {
+                titleFade -= 8;
+                }
+            else {
+                titleFade = 0;
+                }
+            }
+        }
+
+    if( titleFade == 0 ) {
+        stateDone = true;
+        }
+    }
+
+
+
+void PickGameTypeState::drawState() {
+    
+    drawSprite( satelliteTopSpriteID, 
+                0,0, white );
+    drawSprite( satelliteBottomSpriteID, 
+                0,satelliteBottomHalfOffset, white );
+    
+    startNewSpriteLayer();
+    
+    if( titleFade > 0 ) {
+        rgbaColor titleColor = white;
+        titleColor.a = titleFade;
+        
+        drawSprite( titleSpriteID[0], 
+                    0,0, titleColor );
+
+        drawSprite( titleSpriteID[1], 
+                    0,64, titleColor );
+
+        drawSprite( titleSpriteID[2], 
+                    0,80, titleColor );
+        }
+
+    
+    
+    if( !selectionMade ) {
+        
+        aiButton->draw();
+        wifiButton->draw();
+        }
+    
+    }
+
+
+
+
+
+void PickGameTypeState::enterState() {
+    stateDone = false;
+
+    statusMessage = translate( "phaseStatus_gameType" );
+    statusSubMessage = translate( "phaseSubStatus_gameType" ); 
+    
+    selectionMade = false;
+    }
+
+
+
+
+
+
+// singleton
+static PickGameTypeState state;
+
+
+GameState *pickGameTypeState = &state;
+
