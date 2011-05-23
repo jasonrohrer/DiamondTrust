a00760ad6d76424652759625434a7de242117da6
Jason Rohrer <jasonrohrer@fastmail.fm>
2011-05-23 12:25:01 -0600

    Added missing loading.h

---
+++ gameSource/loading.h
@@ -0,0 +1,11 @@
+
+// called by various "init_____" function to add a tick to the progress bar
+// AND redraw the screens (when game loop hasn't started yet)
+void updateLoadingProgress();
+
+
+// JUST redraw the screens (don't add tick to progress bar).
+void redrawLoadingProgress();
+
+
+
