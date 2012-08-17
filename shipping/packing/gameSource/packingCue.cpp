91a90d85dcf9d51925a3cd8a0f763efe8092cf9c
Jason Rohrer <jasonrohrer@fastmail.fm>
2012-08-17 15:48:48 -0700

    Close to working perfectly.

--- shipping/packing/gameSource/packingCue.cpp
+++ shipping/packing/gameSource/packingCue.cpp
@@ -5,10 +5,13 @@
 
 #include "minorGems/game/Font.h"
 
+#include "minorGems/util/random/CustomRandomSource.h"
 
 
 
 Font *mainFont;
+Font *displayFont;
+Font *displayFontFixed;
 // position of view in world
 doublePair lastScreenViewCenter = {0, 0 };
 
@@ -21,6 +24,42 @@ double viewWidth = 20;
 double viewHeightFraction;
 
 double frameRateFactor = 1;
+
+
+
+unsigned int randSeed = 1285702442;
+CustomRandomSource randSource(randSeed);
+
+
+
+#define NUM_SPECIALS 6
+
+SpriteHandle specialPictures[ NUM_SPECIALS ];
+
+
+const char *specialTGAFiles[ NUM_SPECIALS ] = { "largeDiamond.tga",
+                                                "angolanStamp.tga",
+                                                "travellersCheck.tga",
+                                                "angolanBill.tga",
+                                                "belgianCoin.tga",
+                                                "extraDiamond.tga" };
+
+
+int specialCounts[ NUM_SPECIALS ] = { 161,
+                                      403,
+                                      78,
+                                      28,
+                                      63,
+                                      300 };
+
+// specialCounts[s][e] true if special s occurs in envelope e
+char specialDistributions[ NUM_SPECIALS ][1000];
+
+                                                
+
+
+int currentDisplay = 1;
+
 
 
 
@@ -121,6 +160,8 @@ void initFrameDrawer( int inWidth, int inHeight, int inTargetFrameRate,
     
 
     mainFont = new Font( getFontTGAFileName(), 1, 4, false );
+    displayFont = new Font( getFontTGAFileName(), 1, 4, false, 2 );
+    displayFontFixed = new Font( getFontTGAFileName(), 0, 1, true, 2 );
 
 
     setViewCenterPosition( lastScreenViewCenter.x, lastScreenViewCenter.y );
@@ -135,12 +176,64 @@ void initFrameDrawer( int inWidth, int inHeight, int inTargetFrameRate,
     
     
     setViewSize( viewWidth );
+
+
+
+
+    // setup distributions
+
+    for( int i=0; i<NUM_SPECIALS; i++ ) {
+        specialPictures[i] = loadSprite( specialTGAFiles[i], false );
+
+        for( int e=0; e<1000; e++ ) {
+            specialDistributions[i][e] = false;
+            }
+        
+
+
+        int countPlaced = 0;
+        
+
+        while( countPlaced < specialCounts[i] ) {
+            
+            int pick = randSource.getRandomBoundedInt( 0, 999 );
+            
+            while( specialDistributions[i][pick] ) {
+                // already full, pick again
+                pick = randSource.getRandomBoundedInt( 0, 999 );
+                }
+            
+            // found empty, fill
+            specialDistributions[i][pick] = true;
+            
+        
+            countPlaced++;
+            }
+        
+
+        printf( "\n\n\nPlacement for special %d:\n\n", i );
+        
+        for( int e=0; e<1000; e++ ) {
+            printf( "%d ", specialDistributions[i][e] );
+            }
+
+        }
+    printf( "\nDone placing\n" );
+
+
     }
 
 
 
 void freeFrameDrawer() {
     delete mainFont;
+    delete displayFont;
+    delete displayFontFixed;
+    
+    for( int i=0; i<NUM_SPECIALS; i++ ) {
+        freeSprite( specialPictures[i] );
+        }
+    
     }
 
 
@@ -192,7 +285,54 @@ static void drawPauseScreen() {
 
 
 void drawFrame( char inUpdate ) {
+    
 
+    double viewHeight = viewHeightFraction * viewWidth;
+
+    doublePair messagePos = lastScreenViewCenter;
+
+
+    setDrawColor( 1, 1, 1, 1 );
+
+    messagePos = lastScreenViewCenter;
+
+    messagePos.y += 5.75 * ( viewHeight / 15 );
+
+    
+    char *message = autoSprintf( "%d", currentDisplay );
+
+    displayFont->drawString( "Envelope:", messagePos, alignRight );
+
+    setDrawColor( 1, 1, 0.25, 1 );
+
+
+    messagePos.x += 5;
+    displayFontFixed->drawString( message, messagePos, alignRight );    
+
+    delete [] message;
+    
+
+    setDrawColor( 1, 1, 1, 1 );
+
+
+    doublePair picPosition = lastScreenViewCenter;
+    
+    picPosition.x -= 6;
+    picPosition.y += 2;
+    for( int i=0; i<NUM_SPECIALS; i++ ) {
+
+        if( i == NUM_SPECIALS / 2 ) {
+            picPosition.y -= 5;
+            picPosition.x = lastScreenViewCenter.x - 6;
+            }
+        
+        if( specialDistributions[i][ currentDisplay ] ) {
+    
+            drawSprite( specialPictures[i], picPosition, 0.5/16.0 );
+            }
+        picPosition.x += 5;
+        }
+    
 
 
     if( pauseScreenFade > 0 ) {
@@ -248,6 +388,26 @@ void pointerUp( float inX, float inY ) {
 
 
 void keyDown( unsigned char inASCII ) {
+    if( inASCII == ' ' ) {
+        currentDisplay ++;
+        if( currentDisplay > 1000 ) {
+            currentDisplay = 1000;
+            }
+        }
+    else if( inASCII == 'w' || inASCII == 'W' ) {
+        currentDisplay += 100;
+        currentDisplay = ( currentDisplay / 100 ) * 100;
+        if( currentDisplay > 1000 ) {
+            currentDisplay = 1000;
+            }
+        }
+    else if( inASCII == 's' || inASCII == 'S' ) {
+        currentDisplay -= 100;
+        currentDisplay = ( currentDisplay / 100 ) * 100;
+        if( currentDisplay < 1 ) {
+            currentDisplay = 1;
+            }
+        }
     }
 
 
@@ -258,7 +418,34 @@ void keyUp( unsigned char inASCII ) {
 
 
 void specialKeyDown( int inKeyCode ) {
+    if( inKeyCode == MG_KEY_RIGHT ) {
+        currentDisplay ++;
+        if( currentDisplay > 1000 ) {
+            currentDisplay = 1000;
+            }
+        }
+    else if( inKeyCode == MG_KEY_LEFT ) {
+        currentDisplay --;
+        if( currentDisplay < 1 ) {
+            currentDisplay = 1;
+            }
+        }
+    if( inKeyCode == MG_KEY_PAGE_UP ) {
+        currentDisplay += 10;
+        currentDisplay = ( currentDisplay / 10 ) * 10;
+        if( currentDisplay > 1000 ) {
+            currentDisplay = 1000;
+            }
+        }
+    else if( inKeyCode == MG_KEY_PAGE_DOWN ) {
+        currentDisplay -= 10;
+        currentDisplay = ( currentDisplay / 10 ) * 10;
+        if( currentDisplay < 1 ) {
+            currentDisplay = 1;
+            }
+        }
     }
+
 
 
 void specialKeyUp( int inKeyCode ) {
